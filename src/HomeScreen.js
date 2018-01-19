9d4a1b2d5d3e07740f5f4dd1cee8ca183308e0d2
dayudodo <dayudodo@gmail.com>
2018-01-19 14:02:04 +0800

    course to play

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,67 +1,129 @@
 import React, { Component } from "react";
-import { Button, View } from "react-native";
+import {
+  Button,
+  View,
+  FlatList,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight
+} from "react-native";
 import Video from "react-native-video";
-import Sound from 'react-native-sound';
+import Sound from "react-native-sound";
 
+var courses_max = 19;
+var course_names = [];
+var current_media;
 Sound.setCategory("Playback");
-var first = new Sound("secret_garden_01.mp3", Sound.MAIN_BUNDLE, error => {
-  if (error) {
-    console.log("failed to load the sound", error);
-    return;
+for (let index = 1; index <= courses_max; index++) {
+  var double_name;
+  if (index < 10) {
+    double_name = `0${index}`;
+  } else {
+    double_name = `${index}`;
   }
-  // loaded successfully
-  console.log(
-    "duration in seconds: " +
-      first.getDuration() +
-      "number of channels: " +
-      first.getNumberOfChannels()
-  );
-});
+  course_names.push({ key: `secret_garden_${double_name}.mp3` });
+}
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = { title: "Welcome" };
   constructor(props) {
     super(props);
-    this._isMounted = false;
     this.state = {
       paused: true,
       currentTime: 0.0
     }; //因为上一次的图片没有变化，所以有时候图片的onLoad并不会执行！
   }
-  video: Video;
-  onLoad = () => {
-    // this.setState({duration: data.duration});
-    this.setState({
-      paused: false,
-      playDisable: false
+  playmp3() {
+    var first = new Sound("secret_garden_01.mp3", Sound.MAIN_BUNDLE, error => {
+      if (error) {
+        console.log("failed to load the sound", error);
+        return;
+      }
+      // loaded successfully
+      console.log(
+        "duration in seconds: " +
+          first.getDuration() +
+          "number of channels: " +
+          first.getNumberOfChannels()
+      );
     });
-  };
-
-  onProgress = data => {
-    this.setState({
-      currentTime: data.currentTime
+    first.play(success => {
+      if (success) {
+        console.log("successfully finished playing");
+      } else {
+        console.log("playback failed due to audio decoding errors");
+        // reset the player to its uninitialized state (android only)
+        // this is the only option to recover after an error occured and use the player again
+        // first.reset();
+      }
     });
-  };
-
-  onEnd = () => {
-    //播放结束后停止并且重置声音文件，以便重头播放
-    this.setState({ paused: true });
-    this.video.seek(0);
-  };
-  play = () => {
-    //实现Video中音频的播放
-    this.setState({ paused: false });
+  }
+  _renderItem = oneItem => {
+    console.dir(oneItem);
+    let media_name = oneItem.item.key;
+    return (
+      <View style={styles.listBack}>
+        <Button
+          style={styles.item}
+          title={media_name}
+          onPress={() => {
+            console.log(media_name);
+            if (current_media) {
+              current_media.release();
+            }
+            current_media = new Sound(media_name, Sound.MAIN_BUNDLE, error => {
+              if (error) {
+                console.log("failed to load the sound", error);
+                return;
+              }
+              // loaded successfully
+              console.log(
+                "duration in seconds: " +
+                  current_media.getDuration() +
+                  "number of channels: " +
+                  current_media.getNumberOfChannels()
+              );
+              current_media.play(success => {
+                if (success) {
+                  console.log("successfully finished playing");
+                } else {
+                  console.log("playback failed due to audio decoding errors");
+                  // reset the player to its uninitialized state (android only)
+                  // this is the only option to recover after an error occured and use the player again
+                  current_media.reset();
+                }
+              });
+            });
+          }}
+        />
+      </View>
+    );
   };
   render() {
     const { navigate } = this.props.navigation;
-    const soundUrl = ''
+    const soundUrl = "";
     return (
-      <View>
-        <Button
-          title="Go to Jane's profile"
-          onPress={() => navigate("Profile", { name: "Jane" })}
-        />
+      <View style={styles.container}>
+        <Button title="play" onPress={this.playmp3} />
+        <FlatList data={course_names} renderItem={this._renderItem} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column"
+    // alignItems: "center"
+  },
+  listBack: {
+    backgroundColor: "yellow"
+  },
+  item: {
+    fontSize: 18,
+    height: 44,
+    color: "green"
+  }
+});
